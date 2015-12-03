6db4d85fc1b5b7a07824db2e2a4050e6dd685ec0
Jose Miguel Pascua <jmpascua531@gmail.com>
2015-12-02 20:19:20 -0800

    Added Translucency Shader and Friends
    Friends include materials, textures, scripts, and assorted company.

---
+++ TWI_UnityProject/Assets/ExperimentalScripts/Jose/Scripts/SpinMe.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinMe : MonoBehaviour {
+
+	public Vector3 SpinHow = Vector3.zero;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (SpinHow);
+	}
+}
