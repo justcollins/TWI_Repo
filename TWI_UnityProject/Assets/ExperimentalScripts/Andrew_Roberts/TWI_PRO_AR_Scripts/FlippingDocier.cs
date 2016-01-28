1f1592bb833bd72cdca81e06f90fcfd91c0f047b
aroberts5127 <student@AI232LAB0210P14.AICASF.EDMC.STU>
2016-01-27 20:47:52 -0800

    Dossier Switching
    Got to work on the Canvas System for the games Console Readings. Also
    implemented a script that makes use of the 'P' button to switch between
    "pages" of the dossier. Need to work on camera seeing, seeing about
    making the shit with perspective a little less apparent and a little
    more proper for visual representation.

---
+++ TWI_UnityProject/Assets/ExperimentalScripts/Andrew_Roberts/TWI_PRO_AR_Scripts/FlippingDocier.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlippingDocier : MonoBehaviour {
+
+    public Material[] images;
+    public Renderer rend;
+    private int index = 0;
+    
+
+	// Use this for initialization
+	void Start () {
+        rend.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.material = images[index];
+        index++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (index == images.Length)
+            {
+                index = 0;
+                rend.material = images[index];
+            }
+            else
+            {
+                rend.material = images[index];
+            }
+            index++;
+        }
+	}
+}
