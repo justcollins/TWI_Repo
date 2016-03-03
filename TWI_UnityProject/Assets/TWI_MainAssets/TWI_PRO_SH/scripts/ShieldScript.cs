c01f3bb1f8eae8ad4c8ef6e398c9d15f6e15ebeb
sarah <sarahho713@yahoo.com>
2016-03-02 20:24:44 -0800

    Shield Script
    made fixes to the energy script during class

--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_SH/scripts/ShieldScript.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_SH/scripts/ShieldScript.cs
@@ -1,45 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShieldScript : MonoBehaviour 
+public class ShieldScript : MonoBehaviour
 {
-	public GameObject shieldObj;
-    private Collider shieldObj2;
-	private MeshRenderer sShader;
-	private bool showShield = true;
+    public GameObject shieldObj;
+    private Collider shieldCol; //varible of type Collider
+    private MeshRenderer shieldShader; //variable of type MeshRender
+    private bool showShield = true;
 
-	void Start () 
-	{
-		//beginning of game, you should NOT see the shield up
-		sShader = shieldObj.GetComponent<MeshRenderer>();//sShader takes in GameObj's mesh render
-        shieldObj2 = shieldObj.GetComponent<Collider>();
-        sShader.enabled = true;//make shader not rendered on camera
-		//shieldObj.SetActive(true);//turn mesh off, can pass through
-        shieldObj2.enabled = true;
-	}
-	void Update () 
-	{
-		//turn shield's mesh & shader on/off on camera view when Spacebar is pressed
-		if(Input.GetKeyUp("space"))
-		{
-			sShader = shieldObj.GetComponent<MeshRenderer>();
-			sShader.enabled = !sShader.enabled;//false;
-			//shieldObj.SetActive(false);
-            shieldObj2.enabled = false;
-			showShield = !showShield;
+    void Start()
+    {
+        //beginning of game, you should NOT see the shield up
+        shieldShader = shieldObj.GetComponent<MeshRenderer>();//shader takes in GameObj's mesh render
+        shieldCol = shieldObj.GetComponent<Collider>();
+        shieldShader.enabled = false;//make shader not rendered on camera
+        shieldCol.enabled = false;//the obj's collider is unenabled
+    }
+    void Update()
+    {
+        //turn shield's mesh & shader on/off on camera view when Spacebar is pressed
+        if (Input.GetKeyUp("space"))
+        {
+            shieldShader = shieldObj.GetComponent<MeshRenderer>();
+            shieldShader.enabled = !shieldShader.enabled;//it's now True
+            showShield = !showShield;//beginning was True now is False
 
-			if(showShield == false)
-			{
-				sShader.enabled = true;
-				//shieldObj.SetActive(true);
-                shieldObj2.enabled = true;
-			}
-			else
-			{
-				sShader.enabled = false;
-				//shieldObj.SetActive(false);
-                shieldObj2.enabled = false;
-			}
-		}
-	}
+            if (showShield == false)
+            {
+                shieldShader.enabled = true;
+                shieldCol.enabled = true;
+                Debug.Log("Shield ON");
+            }
+            else
+            {
+                shieldShader.enabled = false;
+                shieldCol.enabled = false;
+                Debug.Log("Shield OFF");
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider taggers)
+    {
+        if (taggers.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Something in my shield");
+        }
+    }
+
+    /*void OnTriggerEnter(Collider taggers)
+    {
+        if (taggers.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Something in my shield");
+            //taggers --; something that subtracts taggers' HP
+        }
+    }*/
+
+   /* void OnTriggerExit(Collider taggers)
+    {
+        Debug.Log("It's gone now");
+        //this should ALSO appear if the Enemy died in the shield
+    }*/
 }
