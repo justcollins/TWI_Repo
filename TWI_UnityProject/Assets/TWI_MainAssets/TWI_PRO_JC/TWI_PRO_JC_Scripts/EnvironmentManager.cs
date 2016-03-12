873f3dd37c0901b3810736d0d51e9591e3e0583e
Justin Collins <justcollins@comcast.net>
2016-03-11 23:58:20 -0800

    Environment stuff
    Added an Environment Manager so that we can turn all environments off
    except for the current environment and the previous environment.
    
    There is also a test script and scene to demonstrate how the Environment
    Manager works

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JC/TWI_PRO_JC_Scripts/EnvironmentManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentManager : MonoBehaviour {
+
+    public GameObject startingGroup;
+    public GameObject[] environmentGroups;
+
+    private GameObject currentGroup;
+    private GameObject previousGroup;
+    private bool changeCurGroup;
+
+    public void SetActiveGroups(GameObject newCurrentGroup, GameObject newPreviousGroup) {
+        for(int i = 0; i < environmentGroups.Length; i++) {
+            if(newCurrentGroup.name == environmentGroups[i].name) {
+                currentGroup = newCurrentGroup;
+                Debug.Log("currentGroup: " + currentGroup);
+            }
+
+            if(newPreviousGroup.name == environmentGroups[i].name) {
+                previousGroup = newPreviousGroup;
+                Debug.Log("previousGroup: " + previousGroup);
+            }
+        }
+
+        changeCurGroup = true;
+    }
+
+    public string GetCurrentGroup() {
+        return currentGroup.name;
+    }
+
+	void Start () {
+        SetActiveGroups(startingGroup, startingGroup);
+    }
+	
+	void Update () {
+	    if(changeCurGroup) {
+            ManageVisibleGroup();
+        }
+	}
+
+    private void ManageVisibleGroup() {
+        for(int i = 0; i < environmentGroups.Length; i++) {
+            if(environmentGroups[i].name == currentGroup.name || environmentGroups[i].name == previousGroup.name) {
+                environmentGroups[i].SetActive(true);
+            } else {
+                environmentGroups[i].SetActive(false);
+            }
+        }
+        changeCurGroup = false;
+    }
+
+}
---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JC/TWI_PRO_JC_Scripts/TestEnvironmentScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEnvironmentScript : MonoBehaviour {
+
+    public GameObject environment1;
+    public GameObject environment2;
+    public GameObject environment3;
+    public GameObject environment4;
+    public GameObject environment5;
+
+    private EnvironmentManager environmentManager;
+
+    private void Awake() {
+        environmentManager = GameObject.FindObjectOfType<EnvironmentManager>();
+    }
+
+    private void Start () {
+
+	}
+	
+	private void Update () {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            environmentManager.SetActiveGroups(environment1, environment5);
+        } else if(Input.GetKeyDown(KeyCode.S)) {
+            environmentManager.SetActiveGroups(environment2, environment1);
+        } else if(Input.GetKeyDown(KeyCode.D)) {
+            environmentManager.SetActiveGroups(environment3, environment2);
+        } else if(Input.GetKeyDown(KeyCode.F)) {
+            environmentManager.SetActiveGroups(environment4, environment3);
+        } else if(Input.GetKeyDown(KeyCode.G)) {
+            environmentManager.SetActiveGroups(environment5, environment4);
+        }
+	}
+}
