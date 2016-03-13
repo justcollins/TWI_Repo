04150a9ee5589bf2ea66d98f5b147ecef5e84d55
Justin Collins <justcollins@comcast.net>
2016-03-12 16:34:37 -0800

    Added more lord Renato stuff
    Environment Manager now includes fog render setting changes, where we
    can change its color and intensity
    
    There is also a light lerping script to be put on environment lights so
    they can lerp their intensity and color
    
    And for the ship lighting, we can now change their range, intensity and
    color.

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JC/TWI_PRO_JC_Scripts/LightChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Light))]
+public class LightChanger : MonoBehaviour {
+
+    public Color startingColor;
+    public Color endingColor;
+    public float colorSpeed;
+    [Range(0, 8)]
+    public float startingLightIntensity = 1;
+    [Range(0, 8)]
+    public float endingLightIntensity = 1;
+    public float intensitySpeed = 0;
+
+    private new Light light;
+
+	private void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	private void Update () {
+        LerpLights();
+	}
+
+    private void LerpLights() {
+        if(colorSpeed != 0) {
+            light.color = Color.Lerp(startingColor, endingColor, Mathf.PingPong(Time.time, colorSpeed) / colorSpeed);
+        }
+        if(intensitySpeed != 0) {
+            light.intensity = Mathf.Lerp(startingLightIntensity, endingLightIntensity, Mathf.PingPong(Time.time, intensitySpeed) / intensitySpeed);
+        }
+        
+    }
+}
