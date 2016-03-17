9e4703fe7d020a57c8229af54ac5e73ac2dd8a1c
CroBobi <andrija.zoric@yahoo.co.uk>
2016-03-16 22:05:22 -0700

    shiled stuff and lazers
    lazers updated

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_SH/scripts/ShieldScriptCol.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldScriptCol : MonoBehaviour 
+{
+    //public Collider enemyCol;
+
+    void OnTriggerStay(Collider taggers)
+    {
+        if (taggers.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Something in my shield");
+        }
+    }
+}
