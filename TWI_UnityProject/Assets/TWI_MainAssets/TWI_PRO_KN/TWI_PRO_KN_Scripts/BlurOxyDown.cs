6f62c6fb6107187ab31604cbb71844468f6f38d2
kienngo82 <kienngo82@gmail.com>
2016-04-07 22:00:00 -0700

    Blur On Oxygen Depleation
    It blurs up when the oxygen get low

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_KN/TWI_PRO_KN_Scripts/BlurOxyDown.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class BlurOxyDown : MonoBehaviour 
+{
+	public Submarine_Resources sub;
+	public ColorCorrectionCurves ccc;
+	private float value;
+	public float subt;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sub = GetComponent<Submarine_Resources> ();
+		value = sub.getOxygenLevel();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			sub.setOxygenLevel (-5f);
+			value = sub.getOxygenLevel();
+		}
+
+		if(sub.getOxygenLevel() < 10)
+			Camera.main.GetComponent<ColorCorrectionCurves> ().saturation -= subt;
+	
+	}
+}
