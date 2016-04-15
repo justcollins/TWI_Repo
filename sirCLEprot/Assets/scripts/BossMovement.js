920742da3d5f23b5165aa9f5a7f11e3326599e3d
crazyturtle831 <crazyturtle831@gmail.com>
2016-04-14 19:15:21 -0700

    started rigidbody test
    made the scene for the rigidbody so i can test the script

---
+++ sirCLEprot/Assets/scripts/BossMovement.js
@@ -0,0 +1,71 @@
+﻿#pragma strict
+
+enum Direction {north, south, east, west};
+
+var maxAmount : float; 
+var dir1 : Direction;
+
+var maxY : float;
+var minY : float;
+var stepY : float;
+var stepX : float;
+var maxX : float;
+var minX : float;
+
+function Start () {
+		dir1 = Direction.north;
+
+}
+
+function Update () 
+{
+
+
+		if (dir1 == Direction.north)
+		{
+			if(transform.position.y > maxY)
+			{
+				dir1 = Direction.west;
+				stepX *= -1;
+			}
+		}
+		
+		transform.position.y += stepX;
+			
+		if(dir1 == Direction.west)
+		{
+			if (transform.position.x < minX)
+			{
+				dir1 = Direction.south;
+				stepY *= -1;
+			}
+			
+		}
+			
+		transform.position.x += stepX;
+		
+		
+		if(dir1 == Direction.south)
+		{
+			if(transform.position.y < maxY)
+			{
+			dir1 = Direction.east;
+			stepX *= -1;
+			}
+		}
+		
+		transform.position.y += stepY;
+		
+		if(dir1 == Direction.east)
+		{
+			if(transform.position.x < minX)
+			{
+			dir1 = Direction.south;
+			stepY *= -1;
+			}
+		}
+		
+		transform.position.y += stepY;
+				
+};
+	
