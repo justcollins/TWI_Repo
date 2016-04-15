452132a47c4f5cd9e655d07eb9a15288e4b3e378
DwyerDarren7 <DwyerDarren7@gmail.com>
2016-04-14 21:31:21 -0700

    Added Videos + Play Video Code
    I added 3 videos
    
    I added the code to play the videos, to skip the videos + changes the
    scene, and the Movie Texture which plays the videos.

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_DD/TWI_PRO_DD_scripts/introCutscene.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class introCutscene : MonoBehaviour {
+
+    public MovieTexture movie;
+    public MovieTexture successMovie;
+    public MovieTexture deathMovie;
+    private AudioSource audio;
+
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+
+        StartCoroutine("waitForMovieEnd");
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && movie.isPlaying) {
+            Application.LoadLevel(1);
+        }
+	}
+
+    IEnumerator waitForMovieEnd()
+    {
+        while (movie.isPlaying)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        onMovieEnded();
+    }
+
+    void onMovieEnded() {
+        Application.LoadLevel(1);
+    }
+
+}
