891dcaf84a200ea7aa881cfa9210b58f67a9a966
KaBLaMSon <sarefite@gmail.com>
2016-04-18 13:35:47 -0700

    Aim the laser

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JH/TWI_PRO_JH_Scripts/LaserAim_2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+Laser Aim script by Jonathan Harrington
+
+Rotates laser based on camera rotational value
+    */
+public class LaserAim_2 : MonoBehaviour
+{
+
+    public GameObject mCamera;
+    public Rigidbody objLaser;
+
+    void Start()
+    {
+        //select object to follow camera
+        objLaser = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //rotates laser gameobject based on camera rotation
+        objLaser.transform.Rotate(mCamera.transform.rotation.x,
+                                mCamera.transform.rotation.y,
+                                mCamera.transform.rotation.z);
+
+        
+
+    }
+}
