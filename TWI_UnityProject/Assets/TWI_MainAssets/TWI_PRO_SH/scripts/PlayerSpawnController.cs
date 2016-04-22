baaa9bb13b3f7ca073201838487ccf949d0d7378
sarahho713 <sarahho713@yahoo.com>
2016-04-21 21:58:42 -0700

    spawn player
    player can spawn in different positions randomly

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_SH/scripts/PlayerSpawnController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+/*by: Sarah
+ * stores GameObjects tagged "PlayerSpawn" in an array and randomizes
+ * where the Player will spawn in game space
+ */
+public class PlayerSpawnController : MonoBehaviour 
+{
+	private GameObject[] playerSpawnArray; //initialize an array variable
+	private int randSpawnID;
+
+	void Awake()
+	{
+		//the length of the array is determined by the number of GameObjects tagged "PlayerSpawn"
+		playerSpawnArray = GameObject.FindGameObjectsWithTag ("PlayerSpawn");
+	}
+
+	void Start() 
+	{
+		GetRandomPlayerSpawn();
+	}
+
+	public GameObject GetRandomPlayerSpawn()//it's type GameObject b/c it's accessed by another script
+	{
+		randSpawnID = Random.Range(0,(playerSpawnArray.Length));
+		Debug.Log("SpawnID: " + randSpawnID);
+		return playerSpawnArray[randSpawnID];//returns randSpawnID value to another script called "PlayerSpawning"
+	}
+}
---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_SH/scripts/PlayerSpawning.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+/*by: Sarah
+ * this script handles the actual spawn for our Player
+ */
+public class PlayerSpawning : MonoBehaviour
+{
+	private GameObject player;
+	private GameObject randPlayerSpawn;
+	private PlayerSpawnController playerSpawnCTRL;
+	
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerSpawnCTRL = GameObject.FindGameObjectWithTag ("PlayerSpawnCTRL").GetComponent<PlayerSpawnController>();
+	}
+	
+	void Start() 
+	{
+		//inherits from "PlayerSpawnController" script
+		randPlayerSpawn = playerSpawnCTRL.GetRandomPlayerSpawn();
+		SpawnPlayer();
+	}
+	
+	void SpawnPlayer()
+	{
+		player.transform.position = randPlayerSpawn.transform.position;//accessing Player's pivot point
+		Debug.Log("You have spawn at: " + randPlayerSpawn.name);
+	}
+}
