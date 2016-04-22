a2424cc86415ab001b9150512ba9c66f2f11c405
aroberts5127 <student@AICASF.EDMC.STU>
2016-04-21 19:23:09 -0700

    FUCK THIS MANAGER
    This stupid Active Environment manager isnt working yet. its probably
    something stupid. Will be done soon

--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_AR/TWI_PRO_AR_Scripts/ActiveEnvironments.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_AR/TWI_PRO_AR_Scripts/ActiveEnvironments.cs
@@ -1,19 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ActiveEnvironments : MonoBehaviour {
 
-    private GameObject[] actives;
-    public GameObject startingZone;
+    private GameObject[] allObj;
+    private List<GameObject> envNames;
+    private List<GameObject> activeEnv;
+    public GameObject[] startingEnv;
+   
 	// Use this for initialization
 	void Start () {
-        actives = new GameObject[4];
-        startingZone.SetActive(true);
-        actives[1] = startingZone;
-	}
+        for (int i = 0; i < startingEnv.Length; i++)
+        {
+            addToList(startingEnv[i], i);
+        }
+        activeEnv = new List<GameObject>(5);
+        allObj = new GameObject[200];
+        allObj = FindObjectsOfType<GameObject>();
+        envNames = new List<GameObject>(10);
+        findLayer(8);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        for (int i = 0; i < envNames.Count; i++)
+        {
+            for (int j = 0; j < activeEnv.Count; j++)
+            {
+                if (activeEnv[j].name == envNames[i].name)
+                {
+                    envNames[i].SetActive(true);
+                }
+            }
+        }
 	}
+
+
+    void findLayer(int fLayer)
+    {
+        Debug.Log("fLayer: " + fLayer);
+        for (int i = 0; i < allObj.Length; i++)
+        {
+            Debug.Log("OBJ: " + allObj[i].name + " Layer: " + allObj[i].layer);
+            if (allObj[i].layer == fLayer)
+            {
+                envNames.Add(allObj[i]);
+                Debug.Log("BUTTS");
+            }
+        }
+
+        /*for (int j = 0; j < envNames.Count; j++)
+        {
+            Debug.Log(envNames[j].name);
+        }*/
+    }
+
+    public void addToList(GameObject envItem, int index)
+    {
+        activeEnv.Insert(index, envItem);
+    }
 }
