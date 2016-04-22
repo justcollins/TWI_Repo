761bd8d4d1f3e91b4a658b59c4cc393710a70c1d
kienngo82 <kienngo82@gmail.com>
2016-04-21 22:13:04 -0700

    Oxygen sound effect
    I started to implement the sound effect with the oxygen going down. It
    is a work in progress that works with Unity Audio Mixer. Right now you
    can can control the volume with a slider, but not when it drops down to
    a certain percentage. it works, but it is not subtracting correctly.

--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_KN/TWI_PRO_KN_Scripts/BlurOxyDown.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_KN/TWI_PRO_KN_Scripts/BlurOxyDown.cs
@@ -1,12 +1,22 @@
-﻿using UnityEngine;
+﻿/*
+ * Current problem is the value for the sound is not currently subtracting correctly.
+ * I need a variable for that.
+ * Right now it simply subtract one due to the -- or ++
+ * 
+ */ 
+
+
+using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.ImageEffects;
+using UnityEngine.Audio;
 
 public class BlurOxyDown : MonoBehaviour
 {
     //Get rid of this later and make it private. Our reference only.
     [Header("Submarine Resources")]
     public Submarine_Resources sub_res;
+    public AudioMixer masterMixer;
 
     [Header("Tweak Value")]
     [Tooltip("This is how much you would like to subtract from the Saturation Value")]
@@ -18,10 +28,22 @@ public class BlurOxyDown : MonoBehaviour
     [Range(0.0f, 1.0f)]
     public float percentage = 0.1f;
 
+    public float maxSnd = 20.0f;
+    public float minSnd = -80.0f;
+    public float sndSub = 1.0f;
+
 
     [Tooltip("Minimum Saturation Value that the saturation will go down to.")]
     [Range(0.0f, 1.0f)]
     public float minSaturationValue = 0.0f;
+
+    //Test purpose
+    [Range(-80.0f, 20.0f)]
+    public float bgm = 0.0f;
+
+    [Range(-80.0f, 20.0f)]
+    public float sndTurbine = 0.0f;
+
 
 
     /*
@@ -41,6 +63,7 @@ public class BlurOxyDown : MonoBehaviour
     public float oxygenValue;
     public float saturationValue;
 
+
     // Use this for initialization
     void Start()
     {
@@ -52,7 +75,7 @@ public class BlurOxyDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.P))
             sub_res.setOxygenLevel(-value_per_click);
 
         oxygenValue = sub_res.getOxygenLevel();
@@ -62,21 +85,55 @@ public class BlurOxyDown : MonoBehaviour
         {
             //if oxygen goes below zero we want the saturation to equal only the minimum saturation
             if (oxygenValue <= 0.0f || Camera.main.GetComponent<ColorCorrectionCurves>().saturation < minSaturationValue)
+            {
                 Camera.main.GetComponent<ColorCorrectionCurves>().saturation = minSaturationValue;
+                //SetTurbineLevel(maxSnd);
+            }
             else
+            {
                 Camera.main.GetComponent<ColorCorrectionCurves>().saturation -= subt;
+                //if (sndSub <= maxSnd)
+                //    SetTurbineLevel(sndSub++);
+                //else
+                //    SetTurbineLevel(maxSnd);
+            }
         }
         //If the Oxygen is above a certain percentage add oxygen
         else if (oxygenValue > (sub_res.maxOxygen * percentage))
         {
             if (oxygenValue >= sub_res.maxOxygen)
+            {
                 Camera.main.GetComponent<ColorCorrectionCurves>().saturation = 1.0f;
+               // SetTurbineLevel(minSnd);
+            }
             else
+            {
                 Camera.main.GetComponent<ColorCorrectionCurves>().saturation += subt;
+                //if (sndSub <= minSnd)
+
+                //    SetTurbineLevel(sndSub--);
+                //else
+                //    SetTurbineLevel(minSnd);
+            }
         }
 
         saturationValue = Camera.main.GetComponent<ColorCorrectionCurves>().saturation;
 
+        SetTurbineLevel(sndTurbine);
+        SetMusicLevel(bgm);
+
     }//End of Update()
+
+    public void SetTurbineLevel(float turbinglvl)
+    {
+        //The value string you called in the mixer, value to set it to be
+        masterMixer.SetFloat("Turbine", turbinglvl);
+        
+    }
+
+    public void SetMusicLevel(float musiclvl)
+    {
+       masterMixer.SetFloat("BGM", musiclvl);
+    }
 
 }//End of BlurOxyDown
