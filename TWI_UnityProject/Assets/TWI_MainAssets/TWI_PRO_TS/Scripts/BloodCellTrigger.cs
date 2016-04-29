0f8954940b2287699cefe0ba997f492eb9065b6c
Yogurtt <student@AICASF.EDMC.STU>
2016-04-28 21:13:45 -0700

    Demo Scene for Triggers
    Working on the demo scene that changes the velocity on enter of the
    trigger

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_TS/Scripts/BloodCellTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodCellTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        //ParticleCollisionEvent.velocity
+        //Particle.velocity
+    }
+}
