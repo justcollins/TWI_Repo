b44ce88975eded21d638cc3d04cb4db9873912e3
KaBLaMSon <sarefite@gmail.com>
2016-04-28 20:37:37 -0700

    Try testlaser2 follow

--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JH/TWI_PRO_JH_Scripts/LaserAim_2.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JH/TWI_PRO_JH_Scripts/LaserAim_2.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 /*
-Laser Aim script by Jonathan Harrington
+Laser Aim script 
 
 Rotates laser based on camera rotational value
     */
@@ -10,25 +10,33 @@ public class LaserAim_2 : MonoBehaviour
 {
 
     public GameObject mCamera;
-    public Rigidbody objLaser;
+    public float vSpeed = 5.0f;
+    public float hSpeed = 5.0f;
 
-    void Start()
-    {
-        //select object to follow camera
-        objLaser = GetComponent<Rigidbody>();
+    public float minX = -45.0f;
+    public float maxX = 45.0f;
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
 
-    }
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
 
     // Update is called once per frame
     void Update()
     {
+        yaw += hSpeed * Input.GetAxis("Mouse X");
+        yaw = Mathf.Clamp(yaw, minX, maxX);
 
-        //rotates laser gameobject based on camera rotation
-        objLaser.transform.Rotate(mCamera.transform.rotation.x,
-                                mCamera.transform.rotation.y,
-                                mCamera.transform.rotation.z);
+        pitch -= vSpeed * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, minY, maxY);
 
-        
+        transform.localEulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+
+        /*transform.Rotate(Mathf.Clamp(mCamera.transform.rotation.x,minX,maxX),
+            Mathf.Clamp(mCamera.transform.rotation.y,minY,maxY),
+                                mCamera.transform.rotation.z);*/
 
     }
 }
--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JH/TWI_PRO_JH_Scripts/LaserLookAt.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JH/TWI_PRO_JH_Scripts/LaserLookAt.cs
@@ -12,16 +12,20 @@ laser looks at laserguide
 public class LaserLookAt : MonoBehaviour {
 
     public Transform target;
+    
+
 
 	// Use this for initialization
 	void Start () {
 	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         transform.LookAt(target);
+        
 	
 	}
 }
