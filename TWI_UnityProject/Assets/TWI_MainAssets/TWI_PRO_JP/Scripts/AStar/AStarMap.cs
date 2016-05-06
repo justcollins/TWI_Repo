cbef3697d14bd0c0062b99a943d9331988ebf6c4
Jose Miguel Pascua <student@AICASF.EDMC.STU>
2016-05-05 18:47:51 -0700

    added pathfinding
    a*

---
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_JP/Scripts/AStar/AStarMap.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AStarMap : MonoBehaviour {
+
+    static AStarMap _instance;
+    public static AStarMap instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType<AStarMap>();
+            }
+            return _instance;
+        }
+    }
+
+    public AStarNode[] map;
+    //internal List<AStarNode> path;
+    public bool findMapAutomatically = false;
+
+    public int size {
+        get {
+            return map.Length;
+        }
+    }
+
+    void Awake() {
+        if ((map.Length == 0)&&(findMapAutomatically)) {
+            int msize = transform.childCount;
+            map = new AStarNode[msize];
+
+            for (int i = 0; i < msize; i++) {
+                map[i] = transform.GetChild(i).GetComponent<AStarNode>();
+            }
+        }
+    }
+
+    public AStarNode NodeFromWorldPoint( Vector3 worldPosition ) {
+        foreach (AStarNode n in map) {
+            Vector3 npos = n.worldPosition;
+            if (Vector3.Distance(npos, worldPosition) < n.radius) {
+                return n;
+            }
+        }
+
+        Debug.Log(" OUT OF BOUNDS: Position passed into AStarMap.NodeFromWorldPoint does not fall into any node ");
+        return null;
+    }
+
+    //public void OnDrawGizmos() {
+    //    if (path != null) {
+    //        foreach (AStarNode n in path) {
+    //            Gizmos.color = Color.red;
+    //            Gizmos.DrawSphere(n.worldPosition, 1);
+    //        }
+    //    }
+    //}
+}
