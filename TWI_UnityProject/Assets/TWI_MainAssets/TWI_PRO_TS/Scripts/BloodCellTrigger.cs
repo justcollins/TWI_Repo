2a4a2a68f0d4e6bf60867b34ce81d5111837f0a2
Yogurtt <silk.tori0@gmail.com>
2016-05-05 21:48:49 -0700

    Working on the triggers still
    At a bit of a stand still. Did change from particle emitter to particle
    system. Turns out there is a difference

--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_TS/Scripts/BloodCellTrigger.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_TS/Scripts/BloodCellTrigger.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//If the cockpit enters a trigger the local velocity of the 
+//particle in the mesh emitter should increase/decrease
+//in the "Z" 
+
 public class BloodCellTrigger : MonoBehaviour
 {
+
+    public Vector3 localVelocity;
+    public bool enabled;
+
     void OnTriggerEnter(Collider other)
     {
-        //ParticleCollisionEvent.velocity
-        //Particle.velocity
+    
+        if (other.tag == "BloodParticles")//I'm assuming that the cockpit is the player
+        {
+            ParticleSystem.VelocityOverLifetimeModule.enabled.z = localVelocity;
+        }
     }
 }
