c33d2a2b3c0131db794d6ce030b3034668debc6d
Justin Collins <justcollins@comcast.net>
2016-05-14 15:58:03 -0700

    fixing conflicts
    Took out conflict marks in scripts so Unity and our IDE's will stop
    yelling at us

--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_AZ/TWI_PRO_AZ_Scripts/IntConverter.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_AZ/TWI_PRO_AZ_Scripts/IntConverter.cs
@@ -16,13 +16,7 @@ public class IntConverter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-<<<<<<< HEAD
-//		Locator (BF.currentZone);
-=======
 		//Locator (BF.currentZone);
->>>>>>> master
-
 	}
 
 	public void Locator(GameObject CurZone)
