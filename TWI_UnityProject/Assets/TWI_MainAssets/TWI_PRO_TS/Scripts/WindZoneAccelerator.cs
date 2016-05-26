703964b3292b866423aed990a1cfe61fee895e19
Yogurtt <silk.tori0@gmail.com>
2016-05-25 21:09:43 -0700

    Made progress on the wind zone script
    does more things when its facing backwards or forwards

--- TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_TS/Scripts/WindZoneAccelerator.cs
+++ TWI_UnityProject/Assets/TWI_MainAssets/TWI_PRO_TS/Scripts/WindZoneAccelerator.cs
@@ -19,10 +19,8 @@ public class WindZoneAccelerator : MonoBehaviour {
     private SubControl subThrust;
     private WindZone wind;
  
-    private float LeftBack;
-    private float RightBack;
-    private float LeftFront;
-    private float RightFront;
+    private float LeftCircle;
+    private float RightCircle;
 
 
 	void Start () 
@@ -30,36 +28,107 @@ public class WindZoneAccelerator : MonoBehaviour {
         subThrust = FindObjectOfType<SubControl>();
         thrust = subThrust.thrust;
         wind = GetComponent<WindZone>();
-        LeftBack = Transform.localRotation.x; 
-        /*forwardleft = transform.localRotation + 90;*/
-        //wind.windMain = 5.0f;
+        LeftCircle = transform.localEulerAngles.y - 90;
+        RightCircle = transform.localEulerAngles.y + 90;
+        Debug.Log("Right Point: " + RightCircle);
+        Debug.Log("Left Point: " + LeftCircle);
+        wind.windMain = 5.0f;
+        
 	}
 
     void Update()
     {
         //For the particles matching the ship movement/speed
         thrust = subThrust.thrust;
-        Debug.Log("Thrust: " + thrust);
-        wind.windMain = thrust;
+        //Debug.Log("Thrust: " + thrust);
+        //wind.windMain = thrust;
+        Debug.Log("Local Rotation: " + subThrust.gameObject.transform.localEulerAngles.y);
         
 
         //For the effect of the particles moving more realistically 
         //like a car driving in traffic, the car speeds up and the rest seems to 
         //slow down & the car slows down and the cars around it seem to speed up.
         //The ship and particles needs to be able to reproduce this situtation. 
-      
+        
+        //needs to be done by tomorrow night at the latest tuesday 
+
         //If players forward direction is inbetween these two points then 
         //increase "main" speed of Windzone
  
         //If players forward direction is inbetween these two points then 
         //decrease "main" speed of Windzone
 
-        //For the front... X is greater than 270 and X is less than 90
-        //For the back... X is less than 270 and X is greater than 90
+        //For the front... X is greater than (>=) 270 and X is less than 90(<=)
+        //For the back... X is less than 270 and X is greater than 90 
 
+        //Front Part
+        //if(RightCircle > LeftCircle) {
+        //    Debug.Log("Right is greater than Left");
+        //    if (subThrust.gameObject.transform.localEulerAngles.y <= LeftCircle && subThrust.gameObject.transform.localEulerAngles.y <= RightCircle)
+        //    {
+        //        Debug.Log("Facing Forward");
+        //        wind.windMain = 200;
+        //    }
 
+        //    //Back Part 
+        //    else if (subThrust.gameObject.transform.localEulerAngles.y >= LeftCircle && RightCircle <= subThrust.gameObject.transform.localEulerAngles.y)
+        //    {
+        //        Debug.Log("Facing Backward");
+        //        wind.windMain = 1;
+        //    }
 
-       
+        //    else
+        //    {
+        //        Debug.Log("Well Shit");
+        //    }
+        //} else {
+        //if (RightCircle > LeftCircle)
+        //{
+        //    Debug.Log("why");
+        //    float temp = LeftCircle;
+        //    LeftCircle = RightCircle;
+        //    RightCircle = temp;
+        //}
+            //Debug.Log("Left is greater than Right");
+            //if the ships rotation is greater than the left point IE: > 270 and the ships rotation is lesser than the right point IE: < 90
+        if ((subThrust.gameObject.transform.localEulerAngles.y >= LeftCircle && subThrust.gameObject.transform.localEulerAngles.y >= RightCircle) 
+            || (subThrust.gameObject.transform.localEulerAngles.y <= LeftCircle && subThrust.gameObject.transform.localEulerAngles.y <= RightCircle))
+            {
+                Debug.Log("Facing Forward");
+                wind.windMain = 50; 
+            }
+
+            //Back Part
+            //if the ships rotation is lesser than the left point IE: < 270 and the ships rotation is lesser than the right point IE: < 90
+        else if (subThrust.gameObject.transform.localEulerAngles.y >= LeftCircle && subThrust.gameObject.transform.localEulerAngles.y <= RightCircle)
+            {
+                Debug.Log("Facing Backward");
+                wind.windMain = 1;
+            }
+
+            else
+            {
+                Debug.Log("Well Shit");
+                Debug.Log("Right Point: " + RightCircle);
+                Debug.Log("Left Point: " + LeftCircle);
+            }
+        //}
+
     }
-	
+
+    void OnTriggerEnter(Collider Environment)
+    {
+        if (Environment.gameObject.layer == 8)
+        {
+            Debug.Log("We hit a thing");
+            transform.localEulerAngles = Environment.transform.forward;
+            LeftCircle = transform.localEulerAngles.y - 90;//mess wit these local vs not
+            RightCircle = RightCircle = transform.localEulerAngles.y + 90;
+            Debug.Log("Right Point: " + RightCircle);
+            Debug.Log("Left Point: " + LeftCircle);
+        }
+    }
+
 }
+
+
